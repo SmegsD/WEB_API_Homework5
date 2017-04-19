3e4b0432728ef29c99a5c45c6aea1066995e2ce2
SmegsD <drew.m.smegner@ucdenver.edu>
2017-04-18 22:50:44 -0600

    wrong one uploaded

--- API_Homework5/api/controllers/movies.js
+++ API_Homework5/api/controllers/movies.js
@@ -14,6 +14,7 @@ var Usergrid = new UsergridClient({
     "authMode": "NONE",
     "baseUrl": "https://apibaas-trial.apigee.net",
     "URI": "https://apibaas-trial.apigee.net",
+    'appName' : 'movies',
     "clientId": "b3U61iNCqBvyEeeiwhIuBzeXfQ",
     "clientSecret": "b3U6PbtDfQfxzTAFvVdNoQBRqSjlgz8"
 });
@@ -23,15 +24,84 @@ module.exports = {
     createMovie: createMovie,
     returnMovie: returnMovie,
     movieUpdate: movieUpdate,
-    movieDelete: movieDelete
+    movieDelete: movieDelete,
+    getOne: getOne,
+    addReview: addReview,
+    deleteReview: deleteReview
 
 };
+function addReview (req,res){
+    var review = req.swagger.params.review.value;
+    _.assign(review, {type: 'review'});
+    Usergrid.GET("movies", req.swagger.params.id.value, function(error, usergridResponse, movie) {
+        if (!error){
+            var movietitle = movie.name;
+            //give movie name to review, only if movie exists already
+            _.assign(review, {movie: movietitle});
+            if(_.isUndefined(review.movie))
+                res.json({ Error: "movie undefined."});
+            else if(_.isUndefined(review.score))
+                res.json({ Error: "movie score undefined." });
+            else if(_.isUndefined(review.review))
+                res.json({ Error: "movie review text undefined." });
 
-function getMovies (req,res){
+            else Usergrid.POST(review, function (err, response, review) {
+                    if (err) {
+                        res.json({message: err});
+                    }
+                    else {
+                        review.save(Usergrid, function (err) {
 
-    Usergrid.GET('movies', function(err, response, movie) {
+                            if (err) {
+                                res.status(500).json(err).end();
+                            }
+                            else res.json({
+                                message: 'Review add successful',
+                                review: review
+                            }).end();
+                        });
+                    }
+                })
+        }
+        else
+            res.json({message: "Cannot add review. Movie does not exist.",
+                error: error});
+    });
+}
+function getOne(req,res) {
 
-        if(err){
+    var uuid = req.swagger.params.id.value;
+    var reviews = req.swagger.params.reviews.value;
+    Usergrid.GET("movies", uuid, function (error, usergridResponse, movie) {
+        if (error) {
+            res.json({error: error});
+        }
+        else if (!reviews) {
+            res.json({
+                movie: usergridResponse
+            }).end();
+        }
+        else {
+            var options = {
+                path: "reviews",
+
+                ql: "movie = '" + movie.name + "'"
+            };
+            Usergrid.GET(options, function (error, usergridResponse2) {
+                res.json({
+                    movie: usergridResponse,
+                    reviews: usergridResponse2.entities
+                }).end();
+            })
+        }
+    })
+}
+
+function getMovies (req,res) {
+
+    Usergrid.GET('movies', function (err, response, movie) {
+
+        if (err) {
             res.json({error: err});
         }
         else {
@@ -42,6 +112,20 @@ function getMovies (req,res){
         }
     })
 }
+    function deleteReview(req,res){
+
+        var uuid = req.swagger.params.id.value;
+        Usergrid.DELETE('reviews',uuid, function(error, usergridResponse) {
+            if (error) {
+                res.json({error: error});
+            }
+            else res.json({
+                message: 'Review removed',
+                movie: usergridResponse
+            }).end();
+        })
+    }
+
 function createMovie (req,res){
 
     var movies = req.swagger.params.movie.value;
